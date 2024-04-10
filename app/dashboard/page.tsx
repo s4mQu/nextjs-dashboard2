48ea73b9dda56bac7c122f190d8b08ee519b200c
squintal <squintal@cuttingedge.com.au>
2024-04-10 16:54:22 +1000

    updated

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,11 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 // remenber if in brackets, it means that it is a named export, if not, it is a default export
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
+import {
+  fetchRevenue,
+  fetchLatestInvoices,
+  fetchCardData,
+} from '@/app/lib/data';
 
 // the entire page is an async component..?? we can do that with nextJS. So we can fetch data before rendering the page
 export default async function Page() {
@@ -12,6 +16,13 @@ export default async function Page() {
   // we are then storing that response to a const called revenue.
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
+  // from fetchCardData, we are getting the number of invoices, customers, total paid invoices and total pending invoices from the returned object. now we can use this information to display on the cards.
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
 
   return (
     <main>
@@ -20,14 +31,14 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        /> */}
+        />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
