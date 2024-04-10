683e3b68c696f4633718d9f289394eb1354bd82b
squintal <squintal@cuttingedge.com.au>
2024-04-10 16:03:13 +1000

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+// import the global scope styles from the layout file.
+import '@/app/ui/global.css';
+// import the fonts from the fonts file. add them to the return statement. in a class
+import { inter } from '@/app/ui/fonts';
+
+// the children prop works like the Outlet in react-router-dom.
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
